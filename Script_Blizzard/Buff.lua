af73d4d75ddefdf9ce826f0badc6a0e3c25e2629
KYW <imkyw@outlook.com>
2019-09-30 10:13:58 +0900

    done work buff frame

--- Script_Blizzard/Buff.lua
+++ Script_Blizzard/Buff.lua
@@ -1,4 +1,19 @@
 hooksecurefunc('BuffFrame_UpdateAllBuffAnchors',function()
+    -- Buff Frame
     BuffFrame:ClearAllPoints()
     BuffFrame:SetPoint("TOPRIGHT", Minimap, "TOPLEFT", -10, -3)
+
+    -- Debuff Button
+    for i = 1, 16 do
+        local button = _G['DebuffButton'..i]
+        if not button then break end
+        button:ClearAllPoints()
+        if i == 1 then
+            button:SetPoint("TOPRIGHT", Minimap, "LEFT", -10, 0)
+        elseif i == 9 then
+            button:SetPoint("TOP", DebuffButton1, "BOTTOM", 0, -10)
+        else
+            button:SetPoint("RIGHT", _G['DebuffButton'..i-1], "LEFT", -2, 0)
+        end
+    end
 end)
