71f8653fbaad6aa5171d8a100dc22de0471d4ff9
imKYW <imkyw@outlook.com>
2021-11-30 23:03:15 +0900

    update

--- Script_Extend/RangeDisplay.lua
+++ Script_Extend/RangeDisplay.lua
@@ -1,18 +1,26 @@
 -- Setting
 ---- General
 local ktRD_Font				= "Fonts\\FRIZQT__.ttf"
-local ktRD_FontOutline		= "THINOUTLINE"
+local ktRD_FontOutline		= "THINKOUTLINE"
 ---- Target
-local ktRD_Target_FontSize	= 20
-local ktRD_Target_X       	= -63
-local ktRD_Target_Y       	= -108
+local ktRD_Target_FontSize	= 25
+local ktRD_Target_X       	= -50
+local ktRD_Target_Y       	= 35
 ---- Focus
 local ktRD_Focus_FontSize 	= 16
 local ktRD_Focus_X        	= 172
 local ktRD_Focus_Y        	= 83
 -- /Setting
 
-local ktRD_targetFrame = CreateFrame("Frame", "ktRD_Target", UIParent)
+local ktRD_targetFrame = CreateFrame("Frame", "ktRD_Target", UIParent, BackdropTemplateMixin and 'BackdropTemplate')
+ktRD_targetFrame:SetSize(40, 40)
+ktRD_targetFrame:SetBackdrop({
+    edgeFile = 'Interface\\AddOns\\KhunTark-UI\\Media\\textureGlow', edgeSize = 3,
+    bgFile = 'Interface\\ChatFrame\\ChatFrameBackground',
+    insets = { left = 3, right = 3, top = 3, bottom = 3 } })
+ktRD_targetFrame:SetBackdropColor(0, 0, 0)
+ktRD_targetFrame:SetBackdropBorderColor(0, 0, 0)
+
 local ktRD_focusFrame = CreateFrame("Frame", "ktRD_Focus", UIParent)
 
 local function ktRD_getRangeColor(range)
@@ -31,16 +39,32 @@ local function ktRD_getRangeColor(range)
     return 0.9, 0.9, 0.9
 end
 
+local function ktRD_getRangeBackdropColor(range)
+    if not range or range == 0 then return end
+
+    if range > 20 then
+        return 1, 0, 0
+    elseif range > 8 then
+        return 1.0, 0.82, 0
+    elseif range > 5 then
+        return 0.055, 0.875, 0.825
+    end
+
+    return 0.035, 0.865, 0.0
+end
+
 local function ktRD_OnLoad()
     ktRD_targetFrame:CreateFontString("ktRD_Target_RangeText", "OVERLAY")
+    ktRD_targetFrame:SetPoint("LEFT", UIParent, "CENTER", ktRD_Target_X, ktRD_Target_Y)
+    ktRD_targetFrame:SetAlpha(0)
     ktRD_Target_RangeText:SetFont(ktRD_Font, ktRD_Target_FontSize, ktRD_FontOutline)
-    ktRD_Target_RangeText:SetPoint("LEFT", UIParent, "CENTER", ktRD_Target_X, ktRD_Target_Y)
-    ktRD_Target_RangeText:SetJustifyH("LEFT")
+    ktRD_Target_RangeText:SetPoint("CENTER", ktRD_targetFrame, "CENTER", 1, 0)
+    ktRD_Target_RangeText:SetJustifyH("CENTER")
     ktRD_Target_RangeText:SetText("")
     ktRD_Target_RangeText:Show()
 
     ktRD_focusFrame:CreateFontString("ktRD_Focus_RangeText", "OVERLAY")
-    ktRD_Focus_RangeText:SetFont(ktRD_Font, ktRD_Focus_FontSize, ktRD_FontOutline)
+    ktRD_Focus_RangeText:SetFont(ktRD_Font, ktRD_Focus_FontSize, 'THINOUTLINE')
     ktRD_Focus_RangeText:SetPoint("LEFT", UIParent, "CENTER", ktRD_Focus_X, ktRD_Focus_Y)
     ktRD_Focus_RangeText:SetJustifyH("LEFT")
     ktRD_Focus_RangeText:SetText("")
@@ -61,13 +85,15 @@ function ktRD_Target_OnUpdate(self, elapsed)
             local range = KTL:checkRange("target")
 
             if range == 0 or range > 100 then
-                ktRD_Target_RangeText:SetText("")
+                ktRD_targetFrame:SetAlpha(0)
             else
+                ktRD_targetFrame:SetAlpha(1)
                 ktRD_Target_RangeText:SetText(range)
             end
+            ktRD_targetFrame:SetBackdropColor(ktRD_getRangeBackdropColor(range))
             ktRD_Target_RangeText:SetTextColor(ktRD_getRangeColor(range))
         else
-            ktRD_Target_RangeText:SetText("")
+            ktRD_targetFrame:SetAlpha(0)
         end
     end
 end
@@ -96,11 +122,12 @@ function ktRD_Target_OnEvent(self, event, arg1, arg2, arg3, ...)
     if event == "PLAYER_TARGET_CHANGED" then
         local range = KTL:checkRange("target")
         if range == 0 or range > 100 then
-            ktRD_Target_RangeText:SetText("")
+            ktRD_targetFrame:SetAlpha(0)
         else
+            ktRD_targetFrame:SetAlpha(1)
             ktRD_Target_RangeText:SetText(range)
         end
-
+        ktRD_targetFrame:SetBackdropColor(ktRD_getRangeBackdropColor(range))
         ktRD_Target_RangeText:SetTextColor(ktRD_getRangeColor(range))
     end
 end
